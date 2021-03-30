952ded3e6fa887eb1a1f1f3d7b385f9497206b5e
mathcalim <35414435+mathcalim@users.noreply.github.com>
2021-03-30 20:48:45 +0300

    Delete two_rows.m

--- two_rows.m
+++
@@ -1,9 +0,0 @@
-function y = two_rows(A)
-    if ndims(A) == 2 & height(A) == 2
-        disp('Input was a two-dimensional array')
-        y = A
-    else
-        disp('Input array was not two-dimensional')
-        y = A*0
-    end
-end
